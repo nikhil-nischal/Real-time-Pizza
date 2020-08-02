72a5150414a2205603a42ca14aa6d7871818da5a
nikhil-nischal <2018eeb1169@iitrpr.ac.in>
2020-08-02 20:07:53 +0530

    Initialised directory and server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const expresslayout = require('express-ejs-layouts')
+const path = require('path')
+
+
+
+app.get('/',(req,res) =>{
+    res.render('home')
+})
+
+
+
+app.use(expresslayout)
+app.set('views', path.join(__dirname,'/Resources/Views'))
+app.set('view engine', 'ejs')
+
+
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () =>{
+
+    console.log(`Launching on Port ${PORT}`,)
+
+})
+
+
